12a3169e5b3742f55b3ed77986d5764b9416eae1
akanimoh <oakanimoh@gmail.com>
2023-02-04 20:01:56 +0100

    Adjusted the API code

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-const APIURL = 'http://api.weatherstack.com/current?access_key=e38cb2988109e5bb3049e9c9f5fec0b2&query=';
+const key = '0c0b8f672b46c080c2b3073b3e6a8f3e';
+
+const api_url = (location) => `http://api.weatherstack.com/current?access_key=${key}&query=${location}`;
+/**
+ * The format above uses arrow to declare a variable 
+ * or small funstion and add a value to it 
+ * e.g const greet =(value)=> "Hello" + value;
+ * you call it like this ,
+ * greet("Everyone");
+ * output is "Hello Everyone"
+ */
 
 async function getWeatherByLocation(location) {
-    const resp = await fetch(APIURL+location);
+    const resp = await fetch(api_url(location));
 
     const respData = await resp.json();
     console.log(respData);
